3a2e899178f9fb49b327dfaa01e23d092dcbe7ae
hoangffh <nhathoangx06@gmail.com>
2023-01-11 22:01:20 +0700

    Rebase

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <TheSideBar></TheSideBar>
   <TheContainer></TheContainer>
-  <h1>Task 01 Tran Nhat Hoang 000012</h1>
+  <h1>Task 01 Tran Nhat Hoang 000012 Tran Van Thai</h1>
+  <h1>Task 01 Tran Nhat Hoang 000012 Tran Van Thai</h1>
 </template>
 
 <scrip>
